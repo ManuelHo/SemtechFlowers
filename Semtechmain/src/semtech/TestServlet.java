252b2b04dfbf49fff0a95d03ebd4f1c7d75a8cad
ManuelHo <manuel.hochreiter@atn.ac.at>
2014-05-28 00:06:30 +0200

    added some methods

--- Semtechmain/src/semtech/TestServlet.java
+++ Semtechmain/src/semtech/TestServlet.java
@@ -30,7 +30,9 @@ import org.semanticweb.owlapi.model.OWLObjectPropertyAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import org.semanticweb.owlapi.model.PrefixManager;
+import org.semanticweb.owlapi.model.UnknownOWLOntologyException;
 import org.semanticweb.owlapi.reasoner.ConsoleProgressMonitor;
 import org.semanticweb.owlapi.reasoner.NodeSet;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
@@ -39,6 +41,7 @@ import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
 import org.semanticweb.owlapi.util.DefaultPrefixManager;
 
+import utils.Flower;
 import coreservlets.ServletUtilities;
 
 @WebServlet("/create")
@@ -49,7 +52,7 @@ public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private OWLOntologyManager manager;
-
+	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -88,27 +91,19 @@ public class TestServlet extends HttpServlet {
 		PrefixManager pm = new DefaultPrefixManager(
 				"http://www.semanticweb.org/owltutorial/florist#");
 
-		// Reasoner
-
-		// Create Strauﬂ
-
+		// create strauﬂ
 		OWLClass strauﬂ = fac.getOWLClass(":Strauﬂ", pm);
 		OWLNamedIndividual strauﬂ1 = fac.getOWLNamedIndividual(":Strauﬂ1", pm);
 		OWLClassAssertionAxiom b = fac.getOWLClassAssertionAxiom(strauﬂ,
 				strauﬂ1);
-		int preis = 0;
+		
 		manager.addAxiom(ont, b);
 
-		// create FLowers ///createFlowers
+		// create flowers
 
-		// getParameters into ArrayList
-		ArrayList<String> parameterNames = new ArrayList<String>();
-		Enumeration<String> enumeration = req.getParameterNames();
-		while (enumeration.hasMoreElements()) {
-			String parameterName = (String) enumeration.nextElement();
-			parameterNames.add(parameterName);
-		}
-
+		// get request parameters
+		ArrayList<Flower> flowers = parseRequestFlowers(req);
+		
 		OWLObjectProperty enthaeltBlume = fac.getOWLObjectProperty(
 				":enthaeltBlume", pm);
 		OWLObjectProperty hatFarbe = fac.getOWLObjectProperty(":hatFarbe", pm);
@@ -116,63 +111,50 @@ public class TestServlet extends HttpServlet {
 				":enthaeltThings", pm);
 
 		// create Individuals
-		int zaehler = 0;
+		
 		Set<OWLNamedIndividual> blumenset = new HashSet<OWLNamedIndividual>();
-		for (int i = 0; i < parameterNames.size(); i++) {
-			int count = Integer.parseInt(req.getParameterValues(parameterNames
-					.get(i))[0]);
-			String data = req.getParameterValues(parameterNames.get(i))[1];
-			// flower0 = type, flower1 = color
-			String[] flower = data.split(",");
-			OWLClass type = fac.getOWLClass(":" + flower[0], pm);
-			OWLNamedIndividual farbe = fac.getOWLNamedIndividual(":a"
-					+ flower[1], pm);
-
-			for (int k = 0; k < count; k++) {
-				preis += Double.parseDouble(flower[2]);
-				OWLNamedIndividual blume = fac.getOWLNamedIndividual(":Blume"
+		
+		//counter for individuals
+		int zaehler = 0;
+		double preis = 0;
+		
+		for (int i = 0; i < flowers.size(); i++) {			
+			OWLClass type = fac.getOWLClass(":" + flowers.get(i).getType(), pm);
+			// colors are e.g. arot, ablau, agelb
+			OWLNamedIndividual colour = fac.getOWLNamedIndividual(":a"
+					+ flowers.get(i).getColour(), pm);
+			//create individuals
+			for (int k = 0; k < flowers.get(i).getQuantity(); k++) {
+				OWLNamedIndividual flower = fac.getOWLNamedIndividual(":Blume"
 						+ zaehler, pm);
 				OWLClassAssertionAxiom a = fac.getOWLClassAssertionAxiom(type,
-						blume);
+						flower);
 				manager.addAxiom(ont, a);
-				OWLObjectPropertyAssertionAxiom hatdiesefarbe = fac
-						.getOWLObjectPropertyAssertionAxiom(hatFarbe, blume,
-								farbe);
-				manager.addAxiom(ont, hatdiesefarbe);
-				OWLObjectPropertyAssertionAxiom propertyAssertion = fac
-						.getOWLObjectPropertyAssertionAxiom(enthaeltBlume,
-								strauﬂ1, blume);
-				manager.addAxiom(ont, propertyAssertion);
-				blumenset.add(blume);
+				
+				addObjectProperty(ont, hatFarbe, flower, colour);
+				addObjectProperty(ont, enthaeltBlume, strauﬂ1, flower);
+				
+				blumenset.add(flower);
 				zaehler++;
-
 			}
-
+			preis += (flowers.get(i).getPricePerUnit() * flowers.get(i).getQuantity());
 		}
 
+		// strauﬂ consits of exactly zaehler flowers
 		OWLObjectExactCardinality hasFlowerRestriction = fac
 				.getOWLObjectExactCardinality(zaehler, enthaeltThings);
-
 		OWLAxiom axiom = fac.getOWLClassAssertionAxiom(hasFlowerRestriction,
 				strauﬂ1);
 		manager.addAxiom(ont, axiom);
-		// manager.saveOntology(ont);
 
+		// flowers are different from each other
 		OWLDifferentIndividualsAxiom ia = fac
 				.getOWLDifferentIndividualsAxiom(blumenset);
 		manager.addAxiom(ont, ia);
 
-		// Reasoner
+		OWLReasoner reasoner = initializeReasoner(ont);
 
-		OWLReasonerFactory reasonerFactory = new Reasoner.ReasonerFactory();
-		ConsoleProgressMonitor progressMonitor = new ConsoleProgressMonitor();
-		OWLReasonerConfiguration config = new SimpleConfiguration(
-				progressMonitor);
-		OWLReasoner reasoner = reasonerFactory.createReasoner(ont, config);
-		reasoner.precomputeInferences();
-
-		// Ausgabe erstellen.
-
+		// output
 		StringBuilder sb = new StringBuilder();
 		NodeSet<OWLClass> subClses = reasoner.getTypes(strauﬂ1, true);
 		Set<OWLClass> clses = subClses.getFlattened();
@@ -190,4 +172,44 @@ public class TestServlet extends HttpServlet {
 				"Preis:" + preis + "</body></html>");
 	}
 
+	private OWLReasoner initializeReasoner(OWLOntology ont) {
+		// initialize reasoner
+		OWLReasonerFactory reasonerFactory = new Reasoner.ReasonerFactory();
+		ConsoleProgressMonitor progressMonitor = new ConsoleProgressMonitor();
+		OWLReasonerConfiguration config = new SimpleConfiguration(
+				progressMonitor);
+		OWLReasoner reasoner = reasonerFactory.createReasoner(ont, config);
+		// TODO: check if needed
+		// -- try more than 5 flowers
+		reasoner.precomputeInferences();
+		return reasoner;
+	}
+
+	private void addObjectProperty(OWLOntology ont, OWLObjectProperty property,
+			OWLNamedIndividual domain, OWLNamedIndividual range) {
+		OWLObjectPropertyAssertionAxiom axiom = manager.getOWLDataFactory()
+				.getOWLObjectPropertyAssertionAxiom(property, domain, range);
+		manager.addAxiom(ont, axiom);
+	}
+
+	private ArrayList<Flower> parseRequestFlowers(HttpServletRequest req){
+		Enumeration<String> enumeration = req.getParameterNames();
+		
+		ArrayList<Flower> flowers = new ArrayList<Flower>();
+		
+		while (enumeration.hasMoreElements()) {
+			String parameterName = (String) enumeration.nextElement();
+			int count = Integer.parseInt(req.getParameterValues(parameterName)[0]);
+			
+			String[] params = req.getParameterValues(parameterName)[1].split(",");
+			String type = params[0];
+			String colour = params[1];
+			double price = Double.parseDouble(params[2]);
+			// create flower and add to list
+			flowers.add(new Flower(parameterName, type, colour, price, count));
+		}
+		
+		return flowers;
+	}
+	
 }
---
+++ Semtechmain/src/utils/Flower.java
@@ -0,0 +1,60 @@
+package utils;
+
+public class Flower {
+	String name;
+	String type;
+	String colour;
+	double pricePerUnit;
+	int quantity;
+
+	public Flower(String name, String type, String colour, double pricePerUnit,
+			int quantity) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.colour = colour;
+		this.pricePerUnit = pricePerUnit;
+		this.quantity = quantity;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public double getPricePerUnit() {
+		return pricePerUnit;
+	}
+
+	public void setPricePerUnit(double pricePerUnit) {
+		this.pricePerUnit = pricePerUnit;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
